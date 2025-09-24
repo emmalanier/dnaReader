0c04d5cb66faa96f89c86629838922fb93e3488e
emmalanier <emma.lanier@outlook.fr>
2025-09-24 19:04:52 +0200

    Début de code

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/////////////////////
+//DNA READER - MAIN//
+/////////////////////
+
+//INCLUDES//
+#include <iostream>
+#include <cstring>
+
+//LINKS//
+
+//MAIN PROGRAM//
+
+int main()
+{
+
+  return 0;
+
+}
