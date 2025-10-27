67c7a8197c20cc9c84ed35774f26ce7a070edf6b
emmalanier <emma.lanier@outlook.fr>
2025-10-27 11:22:35 +0100

    SQL table fields

--- includes/databases/elements.sql
+++ includes/databases/elements.sql
@@ -1,3 +1,12 @@
 ----------------------------------
 --SQL DATABASE TO STORE ELEMENTS--
 ----------------------------------
+
+CREATE TABLE elements(
+  elementSymbol VARCHAR PRIMARY KEY,
+  elementName VARCHAR,
+  atomicNumber INT,
+  protonNumber INT,
+  atomicRadius DECIMAL,
+
+);
