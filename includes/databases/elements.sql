0a568b4519a4fdd652a200e0679608f8a70a2a69
emmalanier <emma.lanier@outlook.fr>
2025-10-27 13:37:58 +0100

    Data entries

--- includes/databases/elements.sql
+++ includes/databases/elements.sql
@@ -1,12 +1,26 @@
-----------------------------------
---SQL DATABASE TO STORE ELEMENTS--
-----------------------------------
+------------------------------------
+-- SQL DATABASE TO STORE ELEMENTS --
+------------------------------------
+
+--TABLE CREATION--
 
 CREATE TABLE elements(
   elementSymbol VARCHAR PRIMARY KEY,
   elementName VARCHAR,
   atomicNumber INT,
   protonNumber INT,
-  atomicRadius DECIMAL,
-
+  atomicRadius REAL,
+  electronegativity DECIMAL
 );
+
+
+--ELEMENTS INSERTION--
+INSERT INTO elements VALUES('H', 'Hydrogen', 1, 1, 53E-12, 2.20);
+INSERT INTO elements VALUES('He', 'Helium', 2, 2, 31E-12, 0.0);
+INSERT INTO elements VALUES('Li', 'Lithium', 3, 3, 167E-12, 0.98);
+INSERT INTO elements VALUES('Be', 'Beryllium', 4, 4, 112E-12, 1.57);
+INSERT INTO elements VALUES('B', 'Boron', 5, 5, 87E-12, 2.04);
+INSERT INTO elements VALUES('C', 'Carbon', 6, 6, 67E-12, 2.55);
+INSERT INTO elements VALUES('N', 'Nitrogen', 7, 7, 56E-12, 3.04);
+INSERT INTO elements VALUES('O', 'Oxygen', 8, 8, 48E-12, 3.44);
+INSERT INTO elements VALUES('F', 'Fluorine', 9, 9, 42E-12, 3.98);
