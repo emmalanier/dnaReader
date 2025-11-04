1039b9efc4bbfd19167c1b145a73246280e21165
emmalanier <emma.lanier@outlook.fr>
2025-11-04 21:50:34 +0100

    Beginning of new sql database creation

---
+++ includes/databases/aminoAcids.sql
@@ -0,0 +1,17 @@
+------------------------------------
+-- SQL DATABASE TO STORE ELEMENTS --
+------------------------------------
+
+--TABLE CREATION--
+
+CREATE TABLE if NOT EXISTS aminoAcids(
+  aaAbr TEXT PRIMARY KEY,
+  aaLetter TEXT,
+  aaName TEXT,
+  aa
+  
+
+
+);
+
+INSERT INTO aminoAcids VALUES();
